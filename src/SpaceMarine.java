f802f085233128097b3c276d0d2768f6d2fd7a69
andrey1pozh <andrey1pozh@gmail.com>
2021-05-18 11:27:43 +0300

    Initial commit

---
+++ src/SpaceMarine.java
@@ -0,0 +1,104 @@
+import java.time.LocalDateTime;
+
+public class SpaceMarine implements Comparable<SpaceMarine> {
+    private long id; //Значение поля должно быть больше 0, Значение этого поля должно быть уникальным, Значение этого поля должно генерироваться автоматически
+    private String name; //Поле не может быть null, Строка не может быть пустой
+    private Coordinates coordinates; //Поле не может быть null
+    private LocalDateTime creationDate; //Поле не может быть null, Значение этого поля должно генерироваться автоматически
+    private int health; //Значение поля должно быть больше 0
+    private String achievements; //Поле может быть null
+    private Weapon weaponType; //Поле не может быть null
+    private MeleeWeapon meleeWeapon; //Поле не может быть null
+    private Chapter chapter; //Поле не может быть null
+
+    public SpaceMarine(Long id, String name, Coordinates coordinates, LocalDateTime creationDate, int health,
+                       Weapon weaponType, MeleeWeapon meleeWeapon, Chapter chapter) {
+        this.id = id;
+        this.name = name;
+        this.coordinates = coordinates;
+        this.creationDate = creationDate;
+        this.health = health;
+        this.weaponType = weaponType;
+        this.meleeWeapon = meleeWeapon;
+        this.chapter = chapter;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+    public int getHealth() {
+        return health;
+    }
+    public Weapon getWeaponType() {
+        return weaponType;
+    }
+    public MeleeWeapon getMeleeWeapon() {
+        return meleeWeapon;
+    }
+    public Chapter getChapter() {
+        return chapter;
+    }
+    @Override
+    public int compareTo(SpaceMarine marineObj) {
+        return id.compareTo(marineObj.getId());
+    }
+    @Override
+    public String toString() {
+        String info = "";
+        info += "Солдат №" + id;
+        info += " (добавлен " + creationDate.toLocalDate() + " " + creationDate.toLocalTime() + ")";
+        info += "\n Имя: " + name;
+        info += "\n Местоположение: " + coordinates;
+        info += "\n Здоровье: " + health;
+        info += "\n Дальнее оружие: " + weaponType;
+        info += "\n Ближнее оружие: " + meleeWeapon;
+        info += "\n Орден: " + chapter;
+        return info;
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode() + coordinates.hashCode() + (int) health + weaponType.hashCode() +
+                meleeWeapon.hashCode() + chapter.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj instanceof SpaceMarine) {
+            SpaceMarine marineObj = (SpaceMarine) obj;
+            return name.equals(marineObj.getName()) && coordinates.equals(marineObj.getCoordinates()) &&
+                    (health == marineObj.getHealth())  && (weaponType == marineObj.getWeaponType()) && (meleeWeapon == marineObj.getMeleeWeapon()) &&
+                    chapter.equals(marineObj.getChapter());
+        }
+        return false;
+    }
+
+/*public class Coordinates {
+    private Long x; //Поле не может быть null
+    private Integer y; //Максимальное значение поля: 733, Поле не может быть null
+}
+public class Chapter {
+    private String name; //Поле не может быть null, Строка не может быть пустой
+    private String parentLegion;
+}
+public enum Weapon {
+    COMBI_FLAMER,
+    COMBI_PLASMA_GUN,
+    GRAV_GUN;
+}
+public enum MeleeWeapon {
+    CHAIN_AXE,
+    MANREAPER,
+    LIGHTING_CLAW;
+}*/
+}
