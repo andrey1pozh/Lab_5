c751a8fb8d99433033a5a866c744561ba6113dd6
andrey1pozh <andrey1pozh@gmail.com>
2021-05-19 20:55:44 +0300

    .

---
+++ src/App.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class App {
+    public static final String PS1 = "$ ";
+    public static final String PS2 = "> ";
+
+    public static void main(String[] args) {
+        try (Scanner userScanner = new Scanner(System.in)) {
+            final String envVariable = "LABA";
+
+            MarineAsker marineAsker = new MarineAsker(userScanner);
+            FileManager fileManager = new FileManager(envVariable);
+            CollectionManager collectionManager = new CollectionManager(fileManager);
+            CommandManager commandManager = new CommandManager(
+                new HelpCommand(),
+                new InfoCommand(collectionManager),
+                new ShowCommand(collectionManager),
+                new AddCommand(collectionManager, marineAsker),
+                new UpdateCommand(collectionManager, marineAsker),
+                new RemoveByIdCommand(collectionManager),
+                new ClearCommand(collectionManager),
+                new SaveCommand(collectionManager),
+                new ExitCommand(),
+                new ExecuteScriptCommand(),
+                new AddIfMinCommand(collectionManager, marineAsker),
+                new RemoveGreaterCommand(collectionManager, marineAsker),
+                new HistoryCommand()
+            );
+            Console console = new Console(commandManager, userScanner, marineAsker);
+
+            console.interactiveMode();
+        }
+    }
+}
